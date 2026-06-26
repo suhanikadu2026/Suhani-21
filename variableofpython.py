fdddf2c3bd814115dd381f4fd4d0686f65e78a9d
suhanikadu2026 <suhanik.ml25@sbjit.edu.in>
2026-06-26 20:01:00 +0530

    Add files via upload

---
+++ variableofpython.py
@@ -0,0 +1,48 @@
+name = "suhani"
+age = 19
+price = 10000
+print(name)
+print(age)
+print(price)
+print("My name is : ", name)
+print("My age is : ", age)
+print("my name is : ", name, "and my age is:", age)
+old = False
+a = None
+print(type(old))
+print(type(a))
+#arthematic operators
+a = 10
+b = 12
+print("sum:", a + b)
+print("MUL: ", a*b)
+print("div: ", a / b)
+print(a%b)
+print(a**b)
+#relational operators
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+#assignment operators
+num = 10
+num = num + 5
+print(num)
+num -= 10
+print(num)
+num **= 5
+print(num)
+#logical operators
+x = True
+y = False
+print(not x)
+print(x and y)
+print(x or y)
+"""i am using multi line comments"""
+#type conversion
+a=10
+b = 23.5
+sum = a + b
+print(sum)
