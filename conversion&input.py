c1b617666645615c35d93d71a1eb186c61e8ff94
suhanikadu2026 <suhanik.ml25@sbjit.edu.in>
2026-06-27 19:54:27 +0530

    Add files via upload

---
+++ conversion&input.py
@@ -0,0 +1,32 @@
+a = 23
+b = 4
+c = a / b
+print(c)
+#type conversion
+a = int("25")
+b = int("34")
+print(type(a))
+c = a + b
+print(c)
+a = 3.14
+print(type (a))
+a = str(a)
+print(type(a))
+#input in python
+name = input("Enter your name: ")
+print("welcome", name)
+val = int(input("enter your age: "))
+print(type(val),val)
+val3 = float(input("Price of cetaphil product: "))
+print(type(val3),val3)
+#practice
+val1 = int(input("Enter first number:"))
+val2 = int (input("Enter second number: "))
+sum = val1 + val2
+print(sum)
+val4 = float(input("Enter side of square: "))
+area = val4 * val4
+print(area)
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+print(num1>=num2)
